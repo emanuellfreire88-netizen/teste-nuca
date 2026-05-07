233d87e169f0ffed4e14291e6d3f1f3322838ff7
Emanuell Freire <emanuell.fp.rocha@gmail.com>
2026-05-07 23:42:16 +0000

    Limpeza final - pronto para Vercel

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /home/z/my-project
-while true; do
-  HOSTNAME=0.0.0.0 PORT=3000 node .next/standalone/server.js 2>&1 | tee -a /home/z/my-project/dev.log
-  echo "Server crashed at $(date), restarting in 3s..." >> /home/z/my-project/dev.log
-  sleep 3
-done
--- watchdog.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /home/z/my-project
-while true; do
-  echo "$(date): Starting Next.js server..."
-  node node_modules/.bin/next dev -p 3000
-  EXIT_CODE=$?
-  echo "$(date): Server exited with code $EXIT_CODE, restarting in 2s..."
-  sleep 2
-done
