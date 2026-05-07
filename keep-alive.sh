ba1e8fc093d83c1cef0bcbcf0f38062a291ca336
Emanuell Freire <emanuell.fp.rocha@gmail.com>
2026-05-07 23:41:59 +0000

    Preparado para Vercel - removidos arquivos sensíveis e desnecessários

--- keep-alive.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /home/z/my-project
-while true; do
-  HOSTNAME=0.0.0.0 PORT=3000 bun .next/standalone/server.js 2>&1
-  echo "[$(date)] Server exited, restarting in 2s..." >> /home/z/my-project/crash.log
-  sleep 2
-done
