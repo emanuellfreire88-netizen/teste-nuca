da28645d4c872002fdb60aea3355219b7cba2a8f
Z User <z@container>
2026-06-15 14:16:28 +0000

    de36cb30-dda6-4769-8ab9-ecd5232d6536

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: "standalone", // Not needed for Vercel
+  output: "standalone",
   typescript: {
     ignoreBuildErrors: true,
   },
