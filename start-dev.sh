3657702e51eb516e2b9f73c91a09f77fd2d0aa36
Z User <z@container>
2026-06-24 14:48:32 +0000

    db83479c-e550-4f5c-a166-0a69ae3e90f4

--- start-dev.sh
+++ start-dev.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-# Keep-alive dev server for Nuca Plataforma
 cd /home/z/my-project
-while true; do
-  echo "[$(date)] Starting Next.js dev server..."
-  node node_modules/.bin/next dev -p 3000 -H 0.0.0.0
-  EXIT_CODE=$?
-  echo "[$(date)] Server exited with code $EXIT_CODE. Restarting in 3s..."
-  sleep 3
-done
+exec bun run dev
